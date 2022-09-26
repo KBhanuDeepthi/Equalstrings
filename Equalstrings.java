f61cce680313aebb50e5165cdfd14b6e0ef8ca22
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-26 20:56:04 +0530

    Added: Equal string logic

---
+++ Equalstrings.java
@@ -0,0 +1,13 @@
+class Equalstrings {
+
+	public static void main(String[] args) {
+		String str1 = "Bhanu";
+		String str2 = "Bhanu";
+		if(str1 == str2)
+			System.out.println("Strings are equal");
+		else
+			System.out.println("Strings are not equal");
+
+	}
+
+}
